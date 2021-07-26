6b4450039b7a723fb5d60dd90472b6355673b6a0
Roninho514 <roni.silvasantos514@gmail.com>
2021-07-25 22:04:10 -0300

    Atualização do jogo de forca

--- forca.py
+++ forca.py
@@ -2,5 +2,15 @@ def jogar():
     print("*********************************")
     print("**Bem vindo ao jogo da forca!****")
     print("*********************************")
+    palavra_secreta = 'banana'
+    acertou = enforcou = False
+    while (not acertou and not enforcou):
+        chute = str(input('Qual letra? ')).strip()[0]
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra ):
+                print(f'Encontrei a letra {chute} na posição {index}')
+            index += 1
+        print('Jogando...')
 if(__name__ == '__main__'):
     jogar()
